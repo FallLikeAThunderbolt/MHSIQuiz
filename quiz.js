678219711f084acd1bee0efbc3798c839f11b338
FallLikeAThunderbolt <144525667+FallLikeAThunderbolt@users.noreply.github.com>
2023-09-10 16:23:21 +0530

    Added basic quiz files

---
+++ quiz.js
@@ -0,0 +1,9 @@
+const questions = [
+    // Define your quiz questions here
+];
+
+const quizForm = document.getElementById('quiz-form');
+const submitButton = document.getElementById('submit-button');
+const resultsDiv = document.getElementById('results');
+
+// Add event listeners and logic to handle quiz submissions
